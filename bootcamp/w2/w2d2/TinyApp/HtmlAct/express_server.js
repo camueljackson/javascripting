ac3dd1253e94005b4d5a4237b9bfe1b25bee4671
camueljakcson <your@email.com>
2018-04-18 01:10:05 +0000

    express_server.js is working and generating random string

---
+++ bootcamp/w2/w2d2/TinyApp/HtmlAct/express_server.js
@@ -0,0 +1,59 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080;
+app.set('view engine', 'ejs');
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+    return Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+  }
+
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get('/urls', (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render('urls_index', templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.post("/urls", (req, res) => {
+  var randomString = generateRandomString();
+  console.log(randomString);
+  randomString = req.body["longURL"];
+  console.log(req.body);
+  res.send("Ok");
+});
+
+app.get("/urls/:id", (req, res) => {
+  const shortURL = req.params.id;
+  const templateVars = { shortURL: shortURL,
+                        longURL: urlDatabase[shortURL]
+                        };
+  res.render("urls_show", templateVars); // the short URL ID
+});
+
+app.get("/", (req, res) => {
+  console.log("Hello app.get/")
+  res.end("Hello!");
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
